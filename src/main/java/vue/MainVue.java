f9f612afb3db4b44267661463b5c0b443316df85
Afram <afram.diril@edu.ece.fr>
2022-06-11 16:46:04 +0100

    add sql files + rename controller --> vue interface

--- src/main/java/vue/MainVue.java
+++ src/main/java/vue/MainVue.java
@@ -1,4 +1,4 @@
-package controller;
+package vue;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/vue/VueFXML.java
+++ src/main/java/vue/VueFXML.java
@@ -1,4 +1,4 @@
-package controller;
+package vue;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
