04745675008c6ec32792319500171efdadfcb843
Afram <afram.diril@edu.ece.fr>
2022-06-21 18:17:09 +0100

    - add choice screen admin

---
+++ src/main/java/vue/ChoiceScreenAdmin.java
@@ -0,0 +1,48 @@
+package vue;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChoiceScreenAdmin {
+
+
+
+    @FXML
+    public void listMoviesButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader listMoviesLoader = new FXMLLoader(getClass().getResource("list_movies.fxml"));
+        Parent root = listMoviesLoader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void addMovieButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader addMovieButtton = new FXMLLoader(getClass().getResource("add_movie_one.fxml"));
+        Parent root = addMovieButtton.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void addSessionButtonClicked(ActionEvent event) throws IOException {
+        FXMLLoader addSessionButton = new FXMLLoader(getClass().getResource("add_session.fxml"));
+        Parent root = addSessionButton.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
